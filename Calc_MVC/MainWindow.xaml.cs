a51fd1c933ffd02d34594aad0be6fcd2f306581d
join-devOps <alexander.lebedev2959@mail.ru>
2022-01-26 12:05:45 +0300

    Добавьте файлы проекта.

---
+++ Calc_MVC/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Calc_MVC
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ComboBox_mathsOperation.ItemsSource = Model.OperationsList;
+        }
+
+        private void ComboBox_mathsOperation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Model.IndexOperationList = (byte)ComboBox_mathsOperation.SelectedIndex;
+            Label_mathsOperation.Content = Model.OperationList;
+        }
+
+        private void Button_result_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (TextBox_first.Text != null && TextBox_second.Text != null)
+                {
+                    Model.A = Convert.ToDouble(TextBox_first.Text);
+                    Model.B = Convert.ToDouble(TextBox_second.Text);
+
+                    if (ComboBox_mathsOperation.SelectedIndex == 0)
+                    {
+                        TextBox_result.Text = Model.Sum.ToString();
+                    }
+                    if (ComboBox_mathsOperation.SelectedIndex == 1)
+                    {
+                        TextBox_result.Text = Model.Sub.ToString();
+                    }
+                    if (ComboBox_mathsOperation.SelectedIndex == 2)
+                    {
+                        TextBox_result.Text = Model.Mul.ToString();
+                    }
+                    if (ComboBox_mathsOperation.SelectedIndex == 3)
+                    {
+                        TextBox_result.Text = Model.Del.ToString();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Заполните все поля", "Внимание", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message, "Внимание", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
---
+++ Calc_MVC/Model.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace Calc_MVC
+{
+    class Model
+    {
+        private static List<string> operationsList = new List<string> { "Сложение", "Вычитание", "Умножение", "Деление" };
+        private static List<char> operationList = new List<char> { '+', '-', '*', '/' };
+
+        private static byte index_operationList;
+        private static double a;
+        private static double b;
+
+        public static double A
+        {
+            get { return a; }
+            set
+            {
+                a = value;
+            }
+        }
+
+        public static double B
+        {
+            get { return b; }
+            set
+            {
+                b = value;
+            }
+        }
+
+        public static double Sum
+        {
+            get { return a + b; }
+        }
+
+        public static double Sub
+        {
+            get { return a - b; }
+        }
+
+        public static double Mul
+        {
+            get { return a * b; }
+        }
+
+        public static double Del
+        {
+            get { return a / b; }
+        }
+
+        public static List<string> OperationsList
+        {
+            get { return operationsList; }
+        }
+
+        public static byte IndexOperationList
+        {
+            get { return index_operationList; }
+            set
+            {
+                index_operationList = value;
+            }
+        }
+
+        public static char OperationList
+        {
+            get { return operationList[index_operationList]; }
+        }
+    }
+}
